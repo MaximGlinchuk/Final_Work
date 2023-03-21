abf51d4c04aba511cc5bda4c7050e6acc0697140
Максим Глинчук <glinchuk.maxim@yandex.ru>
2023-03-21 12:11:57 +0400

    create massive

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string[] GetArray(int size)
+{
+    string[] strArray = new string[size];
+    for (int i = 0; i < strArray.Length; i++)
+    {
+        Console.WriteLine("Введите данные в массив");
+        strArray[i] = Console.ReadLine();
+    }
+    return strArray;
+}
